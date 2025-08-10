97f735b672ff801ad9a7513aba70afce0d18da70
Antonio B Neto <93213941+Crowley8881@users.noreply.github.com>
2025-08-10 15:32:11 -0300

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -51,8 +51,33 @@ public class palindrome {
 
 
 
+            
+
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite uma string para verificação:");
+        String texto = scanner.nextLine();
+
+        if (texto.contains("@") && texto.contains(".")) {
+            System.out.println("A string contém os caracteres '@' e '.'");
+        } else if (texto.contains("@")) {
+            System.out.println("A string contém o caractere '@', mas não o '.'");
+        } else if (texto.contains(".")) {
+            System.out.println("A string contém o caractere '.', mas não o '@'");
+        } else {
+            System.out.println("A string não contém os caracteres '@' nem '.'");
+        }
+
+        scanner.close();
+
+
+        
+
+
+
     }
 
 }
+
 
 
