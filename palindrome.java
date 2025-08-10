57aa86c336a4793710ddfc1990ebf05a45458d6d
Antonio B Neto <93213941+Crowley8881@users.noreply.github.com>
2025-08-10 15:24:51 -0300

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -28,7 +28,31 @@ public class palindrome {
 
 
 
+
+
+         Scanner scanner = new Scanner(System.in);
+        String palavra = "";
+
+        do {
+            System.out.println("Insira a senha:");
+
+            palavra = scanner.nextLine();
+
+            if (palavra.equals("alfa")) {
+                System.out.println("Senha correta");
+            } else {
+                System.out.println("Senha incorreta");
+            }
+
+        } while (!palavra.equals("alfa"));
+
+        scanner.close();
+
+
+
+
     }
 
 }
+
 
